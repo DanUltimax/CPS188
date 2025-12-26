b1df04f6ef29a1a1271aabf588e35c684d33fd9f
DanUltimax <danjhewer@gmail.com>
2025-12-25 23:55:03 -0500

    factorial function in hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+int factorial(int num) {
+
+	int num_fac;
+	int i;
+	for (i=1; i <= num; i++) {
+		num_fac *= i;
+	}
+
+	return num_fac;
+}
+
 int main() {
-	printf("Hello CPS188! - Christmas Day, 2025");
+	printf("Hello CPS188! - Christmas Day, 2025\n");
+
+	printf("%d", factorial(6));
+	return 0;
 }
